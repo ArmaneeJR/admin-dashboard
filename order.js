1c6765de7cb9b336a3dc793a6be3ab273ee34e48
Salami Waris <71460131+ArmaneeJR@users.noreply.github.com>
2022-03-13 16:49:22 +0100

    Update

--- order.js
+++ order.js
@@ -1,49 +1,49 @@
 const Orders = [
     {
         productName: 'Ben Sherman Sneakers',
-        productNumber: '34566',
+        productNumber: '&#8358; 34,566',
         paymentStatus: 'Due',
         shipping: 'Pending',
     },
 
     {
         productName: 'Guess Milty Sneakers',
-        productNumber: '43567',
+        productNumber: '&#8358; 43,567',
         paymentStatus: 'Refunded',
         shipping: 'Declined',
     },
     
     {
         productName: 'Blazek Shoe - Brown',
-        productNumber: '56889',
+        productNumber: '&#8358; 56,889',
         paymentStatus: 'Paid',
         shipping: 'Delivered',
     },
 
     {
         productName: 'John Foster  - Black',
-        productNumber: '34789',
+        productNumber: '&#8358; 34,789',
         paymentStatus: 'Due',
         shipping: 'Pending',
     },
 
     {
         productName: 'PRL Small Shirt - Sky Blue',
-        productNumber: '76696',
+        productNumber: '&#8358; 76,696',
         paymentStatus: 'Paid',
         shipping: 'Delivered',
     },
 
     {
         productName: 'Umbro Classic - White',
-        productNumber: '87567',
+        productNumber: '&#8358; 87,567',
         paymentStatus: 'Paid',
         shipping: 'Delivered',
     },
 
     {
         productName: 'Balenciaga Unisex - Black',
-        productNumber: '87567',
+        productNumber: '&#8358; 43,567',
         paymentStatus: 'Refunded',
         shipping: 'Declined',
     },
